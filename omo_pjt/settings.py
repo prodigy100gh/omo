770d79cd06807850ae1da5871e53784127cef289
prodigy100gh <prodigy100@daum.net>
2026-08-25 00:32:32 +0900

    Update settings.py

--- omo_pjt/settings.py
+++ omo_pjt/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = true
 
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
